33c79a2dbe590d8e948297d1a9dc8aee62dc32f6
brutalKim <132333588+brutalKim@users.noreply.github.com>
2025-03-18 11:35:57 +0900

    2025-0.-189

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const viewHeight = window.innerHeight;
+let pageHeigh;
 let scrollY = 0;
 let page = 0;
 let inPageScroll = scrollY/viewHeight;
 const pages = document.querySelectorAll('.page');
+pageHeight = pages[0].offsetHeight;
 document.addEventListener('scroll',function(){
     scrollY = window.scrollY || document.documentElement.scrollTop;
     page = Math.floor(scrollY/viewHeight);
