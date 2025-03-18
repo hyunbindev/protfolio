d71803e4176528e0ef05f1e31d89b4f798aa2801
brutalKim <132333588+brutalKim@users.noreply.github.com>
2025-03-18 11:38:50 +0900

    image change

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-let pageHeigh;
+let pageHeight = 0;
 let scrollY = 0;
 let page = 0;
-let inPageScroll = scrollY/viewHeight;
+let inPageScroll = scrollY/pageHeight;
+
 const pages = document.querySelectorAll('.page');
+
 pageHeight = pages[0].offsetHeight;
 document.addEventListener('scroll',function(){
     scrollY = window.scrollY || document.documentElement.scrollTop;
-    page = Math.floor(scrollY/viewHeight);
-    inPageScroll = scrollY/viewHeight-page;
-    pages[page].style.opacity = 1-inPageScroll
-    pages[page+1].style.opacity = inPageScroll
-    console.log(page);
+    page = Math.floor(scrollY/pageHeight);
+    inPageScroll = scrollY/pageHeight-page;
+    pages[page].style.opacity = 1-inPageScroll;
+    pages[page+1].style.opacity = inPageScroll;
 })
